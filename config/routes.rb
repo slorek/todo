a2cfa600fb805bb6044969840cf9c9ca6cf1881f
Steve Lorek <steve@stevelorek.com>
2014-02-19 15:43:01 +0000

    Add documentation page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Todo::Application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  
+  get 'doc' => 'pages#documentation', as: :documentation
 
   # You can have the root of your site routed with "root"
   root 'pages#index'
