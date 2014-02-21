f8aad44ac2131a752b7089e160efc766b36a3333
Steve Lorek <steve@stevelorek.com>
2014-02-21 10:22:12 +0000

    Add priority/due date fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     return if resource.errors.empty?
     render partial: 'shared/flash', locals: { messages: { error: resource.errors.full_messages } }
   end
+  
+  def priority_options
+    (1..5).to_a
+  end
 end
